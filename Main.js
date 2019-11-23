94e28504f672de1dfd472038bd90397b601ec5a5
happy44300 <happy44300@gmail.com>
2019-11-23 22:31:21 +0100

    Update Main.js

--- Main.js
+++ Main.js
@@ -28,16 +28,19 @@ var crossing = [
 
 //variables d'execution
 var pos = [10,10];
-var gridpos =[0,0]; //[x,y]
+var gridpos =[1,1]; //[x,y]
 var dir = []; //[coord x=0 y=1, pixel increment]
 var Incross = false;
 var points = []; //le joueur se déplace entre 2 points
+
+
 
 function draw(){
  Initialiser();
  MapGridToPixel();
  RectanglePlein(pos[0],pos[1],10,10,'red');
 }
+
 
 function MapGridToPixel()
 {
@@ -46,40 +49,40 @@ function MapGridToPixel()
 
 function bouger(deplacement)
 {
-  //cherchons si le joueur est sur une intersection
+  //sur x
 	if(deplacement[0] == 0)//move on x
     {
-      for(var x in crossing[gridpos[1]][gridpos[0]])
-        {
-          if(x + deplacement >=0 || x + deplacement < x.lenght)
+      var type = crossing[gridpos[1]][gridpos[0]+deplacement[1]];
+      if(gridpos[0] + deplacement[1] >=0 && gridpos[1] + deplacement[1] < crossing[gridpos[1]].lenght && type != 1)
           {
+            Ecrire(deplacement);
             gridpos = [x+deplacement,gridpos[1]];
-          }
         }      
     }
-  draw();
 }
 
-function Keypressed(k){ 
+function Keypressed(k)
+{ 
+  //Ecrire(k);
   
-switch(k){
-  
-   case "38": //up
+  switch(k){
     
+   case 38: //up
+    bouger([1,1]);
     break;
     
-  case "40": // down
-    bouger("down");
+  case 40: // down
+     bouger([1,1]);
     break;
     
-  case "37": //gauche
+  case 37: //gauche
+     bouger([0,-1]);
     break;
     
     
-  case "39": //droite
+  case 39: //droite
     bouger([0,1]);
     break;
-    
-}
   }
+}
 Loop(-1);
