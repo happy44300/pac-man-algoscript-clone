aa2ecce7e1110265bf596591bcfdd31317958174
happy44300 <happy44300@gmail.com>
2019-11-23 20:40:05 +0100

    Update Main.js

---
+++ Main.js
@@ -0,0 +1,23 @@
+var y = 0;
+var x = 0;
+
+function draw() {
+ Initialiser();
+ RectanglePlein(x,y,10,10,'red');
+}
+
+function bouger(deplacement){
+}
+function Keypressed(k){ 
+switch(k){
+  case "36":
+    bouger("enhaut")
+    break;
+  case "37":
+    
+  case "38":
+    
+    
+}
+  }
+Loop(-1);
