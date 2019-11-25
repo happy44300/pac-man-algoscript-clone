f5b7fbb8fd445c99a25b9c315b08bfe0f44538ae
happy44300 <happy44300@gmail.com>
2019-11-25 16:54:30 +0100

    Update Main.js

--- Main.js
+++ Main.js
@@ -2,7 +2,7 @@
 //variable de configuration
 var crossing = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
   [0, 4, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 4, 0],
   [0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0],
   [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
@@ -32,9 +32,6 @@ var scale = 20;
 //variables d'execution
 var playerpos = [10, 10];
 var gridpos = [1, 0]; //[x,y]
-var dir = []; //[coord x=0 y=1, pixel increment]
-var Incross = false;
-var points = []; //le joueur se déplace entre 2 points
 
 Initialiser();
 
@@ -54,41 +51,34 @@ function MapGridToPixel(pos) {
     });
     return pos;
   }
-
   return pos * scale;
 }
 
 function DrawGrid(grid) {
 
-  for (var i = 0; i < grid.length; i++) { //loop on y, first level nested array
-    for (var k = 0; k < grid[i].length; k++) {// on x, second level nested array
+  for (var i = 0; i < grid.length; i++) { 
+    for (var k = 0; k < grid[i].length; k++) {
       if (grid[i][k] == 0 && grid[i][k + 1] == 0) { // is the next slot a wall?
-        Ecrire(MapGridToPixel(k) + " ", MapGridToPixel(i) + " ", MapGridToPixel(k + 1) + " ", MapGridToPixel(i));
         Ligne(MapGridToPixel(k), MapGridToPixel(i), MapGridToPixel(k + 1), MapGridToPixel(i), "red");
       }
-      if (grid[i][k] == 0 && i + 1 < grid.length && grid[i + 1][k] == 0) {
+      if (grid[i][k] == 0 && i + 1 < grid.length && grid[i + 1][k] == 0) { //check that i+1 exist before accesing it
         Ligne(MapGridToPixel(k), MapGridToPixel(i), MapGridToPixel(k), MapGridToPixel(i + 1), "red");
       }
     }
   }
 }
 
-function bouger(deplacement) {
+function bouger(deplacement) { // deplacement is an array with [coord to move, direction on the axis]
   //sur x
   if (deplacement[0] == 0) //move on x
   {
     var type = crossing[gridpos[1]][gridpos[0] + deplacement[1]];
-    //Ecrire(gridpos[0] + deplacement[1] + "first", gridpos[1] + deplacement[1], crossing[gridpos[1]].length, type);
-    Ecrire(gridpos);
     if (gridpos[0] + deplacement[1] >= 0 && gridpos[0] + deplacement[1] < crossing[gridpos[1]].length && type != 0) {
-      //Ecrire("here", crossing[gridpos[1]].length);
       gridpos = [gridpos[0] + deplacement[1], gridpos[1]];
     }
-  } else {
+  } else { //y movement
     var typey = crossing[gridpos[1] + deplacement[1]][gridpos[0]];
-    Ecrire(gridpos);
     if (gridpos[1] + deplacement[1] >= 0 && gridpos[1] + deplacement[1] < crossing.length && typey != 0) {
-      //Ecrire("here", crossing[gridpos[1]].length);
       gridpos = [gridpos[0], gridpos[1] + deplacement[1]];
     }
   }
