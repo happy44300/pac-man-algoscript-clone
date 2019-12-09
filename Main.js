81cc344138ac9fc1de1d3bfff98bc833f8496a2f
happy44300 <happy44300@gmail.com>
2019-12-09 13:11:59 +0100

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,4 +1,3 @@
-// Your code here
 var crossing = [
 
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], //1
@@ -37,10 +36,6 @@ var speed = 0.5;
 turtleEnabled = false;
 
 var GameMap;
-
-
-
-var lastDir = [0,1]; //latest pacman dir, used for rendering
 var initialiser = false;
 
 var intro = ChargerSon('https://happy44300.github.io/intro.wav');
@@ -58,36 +53,37 @@ var spritesheet = PreloadImage("https://happy44300.github.io/sprites32.png");
 
 var GridObject = function() {
 
-    var obj = {};
-    obj.x = 15;
-    obj.y = 15;
-    obj.i = 0;
-    obj.k = 0;
-    obj.states = 0;
-    obj.width = 0;
-    obj.height = 0;
-    obj.offset = 0;
-    obj.dir = [];
-    obj.texture = 0;
-    obj.moving = false;
+  var obj = {};
+  obj.x = 15;
+  obj.y = 15;
+  obj.i = 0;
+  obj.k = 0;
+  obj.states = 0;
+  obj.width = 0;
+  obj.height = 0;
+  obj.offset = 0;
+  obj.dir = [];
+  obj.texture = 0;
+  obj.moving = false;
+  obj.lastDir = [0,1];
 
-    extend(obj, methods);
+  extend(obj, methods);
 
-    return obj;
+  return obj;
 
 };
 
 
 
 var extend = function(obj, methods) {
-    for (var key in methods) {
-        obj[key] = methods[key];
-    }
+  for (var key in methods) {
+    obj[key] = methods[key];
+  }
 };
 var methods = {
-    Pixelpos: function() {
-      return MapGridToPixel([this.k, this.i]);
-    }
+  Pixelpos: function() {
+    return MapGridToPixel([this.k, this.i]);
+  }
 };
 
 
@@ -104,276 +100,287 @@ WaitPreload(ini);
 
 
 function ini() {
-    Initialiser();
-    pacman.width = 15;
-    pacman.height = 15;
-    pacman.offset = -10;
-    pacman.x = MapGridToPixel(9);
-    pacman.y = MapGridToPixel(12);
-    Blinky.i = 10;
-    Blinky.k = 10;
-    Blinky.x = MapGridToPixel(Blinky.i);
-    Blinky.y = MapGridToPixel(Blinky.k);
-    Pinky.i = 9;
-    Pinky.k = 10;
-    Pinky.x = MapGridToPixel(Pinky.i);
-    Pinky.y = MapGridToPixel(Pinky.k);
-    Inky.i = 8;
-    Inky.k = 10;
-    Inky.x = MapGridToPixel(Inky.i);
-    Inky.y = MapGridToPixel(Inky.k);
-    Clyde.i = 7;
-    Clyde.k = 10;
-    Clyde.x = MapGridToPixel(Clyde.i);
-    Clyde.y = MapGridToPixel(Clyde.k);
-    GameMap = crossing.slice();
-    Playsound(1);
-    //setTimeout(function() { iniEnd(); }, 4000);
-    initialiser = true;
+  Initialiser();
+
+  pacman.width = 15;
+  pacman.height = 15;
+  pacman.offset = -10;
+  pacman.x = MapGridToPixel(9);
+  pacman.y = MapGridToPixel(12);
+
+  Blinky.i = 10;
+  Blinky.k = 10;
+  Blinky.x = MapGridToPixel(Blinky.i);
+  Blinky.y = MapGridToPixel(Blinky.k);
+
+  Pinky.i = 9;
+  Pinky.k = 10;
+  Pinky.x = MapGridToPixel(Pinky.i);
+  Pinky.y = MapGridToPixel(Pinky.k);
+
+  Inky.i = 8;
+  Inky.k = 10;
+  Inky.x = MapGridToPixel(Inky.i);
+  Inky.y = MapGridToPixel(Inky.k);
+
+  Clyde.i = 7;
+  Clyde.k = 10;
+  Clyde.x = MapGridToPixel(Clyde.i);
+  Clyde.y = MapGridToPixel(Clyde.k);
+
+  GameMap = crossing.slice();
+  Playsound(1);
+  //setTimeout(function() { iniEnd(); }, 4000);
+  initialiser = true;
 }
 
 function iniEnd() {
-    initialiser = true;
+  initialiser = true;
 }
 
 
 
 function Shortcut(obj) {
 
-    if (obj.x > MapGridToPixel(18) && obj.y > MapGridToPixel(8)) {
-        obj.x = MapGridToPixel(1) + obj.width; //offset
-        obj.y = MapGridToPixel(9) + obj.height;
-    } else if (obj.x < MapGridToPixel(0) && obj.y > MapGridToPixel(8)) {
-        obj.x = MapGridToPixel(17) + obj.width;
-        obj.y = MapGridToPixel(9) + obj.height;
-    }
+  if (obj.x > MapGridToPixel(18) && obj.y > MapGridToPixel(8)) {
+    obj.x = MapGridToPixel(1) + obj.width; //offset
+    obj.y = MapGridToPixel(9) + obj.height;
+  } else if (obj.x < MapGridToPixel(0) && obj.y > MapGridToPixel(8)) {
+    obj.x = MapGridToPixel(17) + obj.width;
+    obj.y = MapGridToPixel(9) + obj.height;
+  }
 }
 
 
 
 function DrawPac(obj) {
-    //each of our sprite is 32*32
-    //ctx.drawImage(spritesheet, 0, 0, x*32, y*32, obj.x - 1/2* obj.width ,obj.y -1/2* obj.height, 32, 32);
-    //DrawImageObject(spritesheet,32,32,32,32);
-    //since we can't load image due to CORS problems in algoscript, we draw pacman by hand
-    var dir = 0;
-    var useddir = [];
-    //since when pacman is stopped, obj.dir is [], we need to assign a value locally
-    if (obj.dir.length == 0) {
-        useddir = lastDir.slice();
+  //each of our sprite is 32*32
+  //ctx.drawImage(spritesheet, 0, 0, x*32, y*32, obj.x - 1/2* obj.width ,obj.y -1/2* obj.height, 32, 32);
+  //DrawImageObject(spritesheet,32,32,32,32);
+  //since we can't load image due to CORS problems in algoscript, we draw pacman by hand
+  var dir = 0;
+  var useddir = [];
+  //since when pacman is stopped, obj.dir is [], we need to assign a value locally
+  if (obj.dir.length == 0) {
+    useddir = obj.lastDir.slice();
+  } else {
+    obj.lastDir = obj.dir.slice();
+    useddir = obj.lastDir.slice();
+  }
+  if (useddir[0] == 0) {
+    if (useddir[1] == 1) {
+      dir = 1;
     } else {
-        lastDir = obj.dir.slice();
-        useddir = lastDir.slice();
+      dir = -1;
     }
-    if (useddir[0] == 0) {
-        if (useddir[1] == 1) {
-            dir = 1;
-        } else {
-            dir = -1;
-        }
+  }
+  if (useddir[0] == 1) {
+    if (useddir[1] == 1) {
+      dir = 1;
+    } else {
+      dir = -1;
     }
-    if (useddir[0] == 1) {
-        if (useddir[1] == 1) {
-            dir = 1;
-        } else {
-          dir = -1;
-        }
-    }
-    ctx.beginPath();
-    ctx.arc(obj.x + pacman.width / 2, obj.y + pacman.height / 2, 10, dir * 0.25 * Math.PI, dir * 1.25 * Math.PI, false);
-    ctx.fillStyle = "rgb(255, 255, 0)";
-    ctx.fill();
-    ctx.beginPath();
-    ctx.arc(obj.x + pacman.width / 2, obj.y + pacman.height / 2, 10, dir * 0.75 * Math.PI, dir * 1.75 * Math.PI, false);
-    ctx.fill();
+  }
+  ctx.beginPath();
+  ctx.arc(obj.x + pacman.width / 2, obj.y + pacman.height / 2, 10, dir * 0.25 * Math.PI, dir * 1.25 * Math.PI, false);
+  ctx.fillStyle = "rgb(255, 255, 0)";
+  ctx.fill();
+  ctx.beginPath();
+  ctx.arc(obj.x + pacman.width / 2, obj.y + pacman.height / 2, 10, dir * 0.75 * Math.PI, dir * 1.75 * Math.PI, false);
+  ctx.fill();
 }
 
 
 
 function draw() {
 
-    Initialiser();
+  Initialiser();
 
-    RectanglePlein(0, 0, 10000, 10000, "black"); //background
-    DrawGrid(GameMap);
+  RectanglePlein(0, 0, 10000, 10000, "black"); //background
+  DrawGrid(GameMap);
 
-    //don't draw or move before everything is loaded
-    if (initialiser == true) {
-        move(pacman);
-        //RectanglePlein(pacman.x, pacman.y, 10, 10, 'yellow');
-        DrawPac(pacman);
-        RectanglePlein(Blinky.x, Blinky.y, 10, 15, "red");
-        RectanglePlein(Pinky.x, Pinky.y, 10, 15, "pink");
-        RectanglePlein(Inky.x, Inky.y, 10, 15, "blue");
-        RectanglePlein(Clyde.x, Clyde.y, 10, 15, "orange");
-        Shortcut(pacman);
-
-    }
-
+  //don't draw or move before everything is loaded
+  if (initialiser == true) {
+    move(pacman);
+    //RectanglePlein(pacman.x, pacman.y, 10, 10, 'yellow');
+    DrawPac(pacman);
+    RectanglePlein(Blinky.x, Blinky.y, 10, 15, "red");
+    RectanglePlein(Pinky.x, Pinky.y, 10, 15, "pink");
+    RectanglePlein(Inky.x, Inky.y, 10, 15, "blue");
+    RectanglePlein(Clyde.x, Clyde.y, 10, 15, "orange");
+    Shortcut(pacman);
+  }
 }
 
 
 
-function BasicIA() {}
+function BasicIA(obj) {
+  
+  var col; //for collision
+  
+  
+  
+}
 
 
 function MapGridToPixel(pos) {
-    if (Array.isArray(pos)) {
-        pos = gridpos.map(function(x) {
-            return x * scale;
-        });
-        return pos;
-    }
-    return pos * scale;
+  if (Array.isArray(pos)) {
+    pos = gridpos.map(function(x) {
+      return x * scale;
+    });
+    return pos;
+  }
+  return pos * scale;
 }
 
 function MapPixelToGrid(pos) {
-    return Math.abs(Math.round((pos / scale)));
+  return Math.abs(Math.round((pos / scale)));
 }
+
 function win() {
-    Ecrire("win");
-    ini();
+  Ecrire("win");
+  ini();
 }
+
 function DrawGrid(grid) {
-    if (grid == undefined) {
-        return;
+  if (grid == undefined) {
+    return;
+  }
+  var gome = false;
+  for (var i = 0; i < grid.length; i++) {
+    for (var k = 0; k < grid[i].length; k++) {
+      if (grid[i][k] == 0 && grid[i][k + 1] == 0) { // is the next slot a wall?
+        Ligne(MapGridToPixel(k), MapGridToPixel(i), MapGridToPixel(k + 1), MapGridToPixel(i), "blue");
+      }
+      if (grid[i][k] == 0 && i + 1 < grid.length && grid[i + 1][k] == 0) { //check that i+1 exist before accesing it
+        Ligne(MapGridToPixel(k), MapGridToPixel(i), MapGridToPixel(k), MapGridToPixel(i + 1), "blue");
+      }
+      if (grid[i][k] == 1 || grid[i][k] == 4) //draw gome
+      {
+        RectanglePlein(MapGridToPixel(k), MapGridToPixel(i), 5, 5, "white");
+        gome = true; //win check here because we loop here
+      }
     }
-    var gome = false;
-    for (var i = 0; i < grid.length; i++) {
-        for (var k = 0; k < grid[i].length; k++) {
-            if (grid[i][k] == 0 && grid[i][k + 1] == 0) { // is the next slot a wall?
-                Ligne(MapGridToPixel(k), MapGridToPixel(i), MapGridToPixel(k + 1), MapGridToPixel(i), "blue");
-            }
-            if (grid[i][k] == 0 && i + 1 < grid.length && grid[i + 1][k] == 0) { //check that i+1 exist before accesing it
-                Ligne(MapGridToPixel(k), MapGridToPixel(i), MapGridToPixel(k), MapGridToPixel(i + 1), "blue");
-            }
-            if (grid[i][k] == 1 || grid[i][k] == 4) //draw gome
-            {
-                RectanglePlein(MapGridToPixel(k), MapGridToPixel(i), 5, 5, "white");
-                gome = true; //win check here because we loop here
-            }
-        }
-    }
-    if (gome == false) {
-        win();
-    }
+  }
+  if (gome == false) {
+    win();
+  }
 }
 
 function collision(obj) {
-    var clipOffset = obj.offset;
-    var clipWidth = obj.width * 2;
-    var clipDepth = obj.height * 2;
-  
-    // extend hitboxe in the direction we are moving so that we don't get stuck when stopping
-    if (obj.dir.length != 0 && obj.dir[0] == 0) {
-        if (obj.dir[1] > 0) {
-            clipWidth = clipWidth * obj.dir[1] + 4;
-        } else {
-            clipWidth = obj.dir[1] - 2;
-        }
-    } else if (obj.dir[0] == 1) {
-        if (obj.dir[1] > 0) {
-            clipDepth = clipDepth * obj.dir[1] + 4;
-        } else {
-            clipDepth = obj.dir[1] - 2;
-        }
-    }
-    var clipLength = clipWidth * clipDepth;
-  
-    //extract image data from the canvas around the player
-    var color = ctx.getImageData(obj.x + clipOffset, obj.y + clipOffset, clipWidth, clipDepth);
-    var gomehitbox = ctx.getImageData(obj.x - obj.width / 2, obj.y - obj.height / 2, obj.width * 2, obj.height * 2);
-    for (i = 0; i < gomehitbox.data.length; i += 8) {
-        if (gomehitbox.data[i] == 255 && gomehitbox.data[i + 1] == 255 && gomehitbox.data[i + 2] == 255) {
+  var clipOffset = obj.offset;
+  var clipWidth = obj.width * 2;
+  var clipDepth = obj.height * 2;
 
-            if (GameMap[MapPixelToGrid(obj.y)][MapPixelToGrid(obj.x)] != 0) {
-
-                GameMap[MapPixelToGrid(obj.y)][MapPixelToGrid(obj.x)] = 2;
-                break;
-            }
-        }
+  // extend hitboxe in the direction we are moving so that we don't get stuck when stopping
+  if (obj.dir.length != 0 && obj.dir[0] == 0) {
+    if (obj.dir[1] > 0) {
+      clipWidth = clipWidth * obj.dir[1] + 4;
+    } else {
+      clipWidth = obj.dir[1] - 2;
     }
-    for (var i = 0; i < color.data.length; i += 8) {
-        if (color.data[i + 2] > 100 && color.data[i] != 255) {
-            return true;
-        }
-
+  } else if (obj.dir[0] == 1) {
+    if (obj.dir[1] > 0) {
+      clipDepth = clipDepth * obj.dir[1] + 4;
+    } else {
+      clipDepth = obj.dir[1] - 2;
     }
+  }
+  var clipLength = clipWidth * clipDepth;
+
+  //extract image data from the canvas around the player
+  var color = ctx.getImageData(obj.x + clipOffset, obj.y + clipOffset, clipWidth, clipDepth);
+  var gomehitbox = ctx.getImageData(obj.x - obj.width / 2, obj.y - obj.height / 2, obj.width * 2, obj.height * 2);
+  for (i = 0; i < gomehitbox.data.length; i += 8) {
+    if (gomehitbox.data[i] == 255 && gomehitbox.data[i + 1] == 255 && gomehitbox.data[i + 2] == 255) {
+
+      if (GameMap[MapPixelToGrid(obj.y)][MapPixelToGrid(obj.x)] != 0) {
+
+        GameMap[MapPixelToGrid(obj.y)][MapPixelToGrid(obj.x)] = 2;
+        break;
+      }
+    }
+  }
+  for (var i = 0; i < color.data.length; i += 8) {
+    if (color.data[i + 2] > 100 && color.data[i] != 255) {
+      return true;
+    }
+
+  }
 
 }
 
 function move(obj) { // dir is an array with [coord to move, direction on the axis]
-    if (collision(obj) == true) {
-        obj.dir = [];
-    }
-    if (obj.dir.length == 0) {
-        return obj;
-    }
-    if (obj.dir[0] == 0) //move on x
-    {
-        if (obj.dir[1] == 1) {
-            obj.x += speed;
-        } else {
-            obj.x -= speed;
-        }
-    }
-    if (obj.dir[0] == 1) { //move on y
-        if (obj.dir[1] == 1) {
-            obj.y += speed;
-        } else {
-            obj.y -= speed;
-        }
-    }
+  if (collision(obj) == true) {
+    obj.dir = [];
+  }
+  if (obj.dir.length == 0) {
     return obj;
+  }
+  if (obj.dir[0] == 0) //move on x
+  {
+    if (obj.dir[1] == 1) {
+      obj.x += speed;
+    } else {
+      obj.x -= speed;
+    }
+  }
+  if (obj.dir[0] == 1) { //move on y
+    if (obj.dir[1] == 1) {
+      obj.y += speed;
+    } else {
+      obj.y -= speed;
+    }
+  }
+  return obj;
 }
+
 function Keypressed(k) {
-    if (initialiser == false){
-        return;
-    }
-    switch (k) {
-        case 38:
-            //up
-            pacman.dir = [1, -1];
-            break;
-        case 40:
-            // down
-            pacman.dir = [1, 1];
-            break;
-        case 37:
-            //gauche
-            pacman.dir = [0, -1];
-            break;
-        case 39:
-            //droite
-            pacman.dir = [0, 1];
-            break;
-        case 81:
-            // emergency stop
-            Stop();
-    }
+  if (initialiser == false) {
+    return;
+  }
+  switch (k) {
+  case 38:
+    //up
+    pacman.dir = [1, -1];
+    break;
+  case 40:
+    // down
+    pacman.dir = [1, 1];
+    break;
+  case 37:
+    //gauche
+    pacman.dir = [0, -1];
+    break;
+  case 39:
+    //droite
+    pacman.dir = [0, 1];
+    break;
+  case 81:
+    // emergency stop
+    Stop();
+  }
 
 }
-
-
 
 function Playsound(ost) //fonction qui permet de joue les ost au moment voulut
 {
-    switch (ost) {
-        case 1:
-            intro.play();
-            break;
-        case 2:
-            fantomes.play();
-            break;
-        case 3:
-            waka.play();
-            break;
-        case 4:
-            win.play();
-            break;
-        case 5:
-            lose.play();
-            break;
-    }
+  switch (ost) {
+  case 1:
+    intro.play();
+    break;
+  case 2:
+    fantomes.play();
+    break;
+  case 3:
+    waka.play();
+    break;
+  case 4:
+    win.play();
+    break;
+  case 5:
+    lose.play();
+    break;
+  }
 }
 Loop(-1);
